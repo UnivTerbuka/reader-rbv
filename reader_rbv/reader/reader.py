19c17fae8fd5ce5c54e04164aea2b971ad7dec74
hexatester <revolusi147id@gmail.com>
2021-02-24 15:32:46 +0700

    Add logging

--- reader_rbv/reader/reader.py
+++ reader_rbv/reader/reader.py
@@ -31,7 +31,11 @@ class Reader:
     @cachedmethod(attrgetter("cache"))
     def get(self, kode: str) -> Buku:
         params = {"modul": kode}
+        self.logger.debug(f"Mencari ketersediaan buku {kode}")
         res = self.session.get(self.base, params=params)
         if not res.ok or not res.text:
+            self.logger.warning(f"Buku {kode} tidak ditemukan")
             raise BookNotFound(f"Buku dengan kode {kode} tidak ditemukan")
+        else:
+            self.logger.debug(f"Buku {kode} ditemukan")
         return Buku(kode, self.base, self.session)

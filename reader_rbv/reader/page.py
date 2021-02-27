6e27db21a6203443f46c8228209e22a1f00cb747
hexatester <revolusi147id@gmail.com>
2021-02-27 18:23:17 +0700

    Add Page.from_jsonp

--- reader_rbv/reader/page.py
+++ reader_rbv/reader/page.py
@@ -1,7 +1,8 @@
 import attr
+import ujson as json
 from typing import List
 
-from . import Font
+from . import Font, Text
 
 
 @attr.dataclass(slots=True)
@@ -11,4 +12,21 @@ class Page:
     height: int
     width: int
     fonts: List[Font] = attr.field(factory=list)
-    text: List[list] = attr.field(factory=list)
+    text: List[Text] = attr.field(factory=list, converter=Text.from_list)  # type: ignore
+
+    @classmethod
+    def from_jsonp(cls, jsonp: str) -> List["Page"]:
+        pages_data = json.loads(jsonp)
+        results: List["Page"] = list()
+        for page_data in pages_data:
+            results.append(
+                Page(
+                    number=page_data["number"],
+                    pages=page_data["pages"],
+                    height=page_data["height"],
+                    width=page_data["width"],
+                    fonts=page_data["fonts"],
+                    text=page_data["text"],
+                )
+            )
+        return results

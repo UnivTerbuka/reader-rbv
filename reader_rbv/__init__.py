ef9b958440004037eb077aa36e9ed3d8424478ff
hexatester <revolusi147id@gmail.com>
2021-03-09 20:07:01 +0700

    Move .reader to root

--- reader_rbv/__init__.py
+++ reader_rbv/__init__.py
@@ -1,5 +1,24 @@
-from .reader import Font, Text, Page, Modul, Reader
+from .font import Font
+from .text import Text
+from .page import Page
+from .page_cache import PageCache
+from .modul import Modul
+from .buku import Buku
+from .buku_cache import BukuCache
+
+from .reader import Reader
 
 from .version import __version__  # NOQA
 
-__all__ = ["Font", "Text", "Page", "Modul", "Reader"]
+__author__ = "hexatester <habibrohman@protonmail.com>"
+
+__all__ = [
+    "Font",
+    "Text",
+    "Page",
+    "PageCache",
+    "Modul",
+    "Buku",
+    "BukuCache",
+    "Reader",
+]

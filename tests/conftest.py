8ee1baa6c02afdbb013089a017d6dbc5622542ce
hexatester <revolusi147id@gmail.com>
2021-03-09 21:45:56 +0700

    Disable cache, add base_url

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 from pytest import fixture
 from dotenv import load_dotenv
 
-from reader_rbv import Reader
+from reader_rbv import Reader, BukuCache
 
 load_dotenv()
 
@@ -19,5 +19,15 @@ def password() -> str:
 
 
 @fixture
-def reader(username: str, password: str) -> Reader:
-    return Reader(username, password)
+def base_url() -> str:
+    return os.environ.get("URL_RBV", "")
+
+
+@fixture
+def reader(username: str, password: str, base_url: str) -> Reader:
+    return Reader(
+        username=username,
+        password=password,
+        cache=BukuCache(0, 600),
+        base=base_url,
+    )

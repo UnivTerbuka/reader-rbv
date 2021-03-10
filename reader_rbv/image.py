d9c4f8acebb362923b131bdb3a5607418fd4f2eb
hexatester <revolusi147id@gmail.com>
2021-03-10 16:21:32 +0700

    Initial Image.download

--- reader_rbv/image.py
+++ reader_rbv/image.py
@@ -1,5 +1,13 @@
 import attr
+import logging
+import os
+from requests import Session
+from typing import Optional
 from urllib.parse import urlencode
+
+from .utils import get_default_dir, get_url
+
+logger = logging.getLogger(__name__)
 
 
 @attr.dataclass(slots=True)
@@ -8,13 +16,51 @@ class Image:
     subfolder: str
     page: int
     base: str
+    filepath: Optional[str] = None
+
+    def download(self, session: Session, username: str, password: str) -> str:
+        if self.is_exist():
+            logger.warning(f"Overwriting {self.filepath}!")
+        url = self.url
+        res = get_url(
+            session=session,
+            url=url,
+            username=username,
+            password=password,
+            headers=self._headers,
+            stream=True,
+        )
+        filepath = self._filepath
+        with open(filepath, "wb") as fp:
+            for chunk in res.iter_content(1024):
+                fp.write(chunk)
+        self.filepath = filepath
+        return filepath
 
     @property
     def url(self) -> str:
-        query = {
+        return self.base + "services/view.php?" + urlencode(self._query)
+
+    def is_exist(self, filepath: Optional[str] = None) -> bool:
+        filepath = filepath or self.filepath
+        if filepath:
+            return os.path.isfile(filepath)
+        return False
+
+    @property
+    def _filepath(self) -> str:
+        base = get_default_dir()
+        return os.path.join(base, self.subfolder, f"{self.doc}-{self.page}.jpg")
+
+    @property
+    def _headers(self) -> dict:
+        return {"Referer": self.base + f"index.php?modul={self.doc}"}
+
+    @property
+    def _query(self) -> dict:
+        return {
             "doc": self.doc,
             "format": "jpg",
             "subfolder": self.subfolder,
             "page": self.page,
         }
-        return self.base + "services/view.php?" + urlencode(query)

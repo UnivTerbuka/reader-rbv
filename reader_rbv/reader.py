7bd06e3f5aac93de1d0ffdf03f671e5b5b4f65cf
hexatester <revolusi147id@gmail.com>
2021-04-06 23:19:12 +0700

    Deprecate .get_buku

--- reader_rbv/reader.py
+++ reader_rbv/reader.py
@@ -1,11 +1,12 @@
 import logging
+import warnings
 
 from cachetools import cachedmethod
 from operator import attrgetter
 from requests import Session
 from typing import Mapping, MutableMapping, Optional
 
-from reader_rbv.exception import BookNotFound
+from reader_rbv.exception import BookNotFound, Unreachable
 from . import Book, BookCache
 from .constants import HEADERS
 
@@ -29,8 +30,16 @@ class Reader(Mapping[str, Book]):
             self.logger.debug(".cache disabled")
         self.session.headers.update(HEADERS)
 
-    @cachedmethod(attrgetter("cache"))
     def get_buku(self, code: str) -> Book:
+        warnings.warn(
+            "Deprecated Reader.get_buku in favour of Reader.get_book",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        return self.get_book(code)
+
+    @cachedmethod(attrgetter("cache"))
+    def get_book(self, code: str) -> Book:
         if self.cache is not None:
             cached_book = Book.from_cache(
                 code=code,
@@ -44,7 +53,9 @@ class Reader(Mapping[str, Book]):
         params = {"modul": code}
         self.logger.debug(f"Finding book with code = {code}")
         res = self.session.get(self.base, params=params)
-        if not res.ok or not res.text:
+        if not res.ok:
+            raise Unreachable("Server unreachable")
+        elif not res.text:
             raise BookNotFound(f"Unable to find book with code = {code}")
         else:
             self.logger.debug(f"Found book with code = {code}")
@@ -62,7 +73,7 @@ class Reader(Mapping[str, Book]):
                 self.cache.popitem()
 
     def __getitem__(self, key: str) -> Book:
-        return self.get_buku(key)
+        return self.get_book(key)
 
     def __len__(self):
         return len(self.cache)

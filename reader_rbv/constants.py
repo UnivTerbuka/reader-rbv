e45e477f20ecb7bccf0c1afb79cd5e4133f6a747
hexatester <hexatester@protonmail.com>
2022-09-15 23:08:49 +0700

    Update HEADERS

--- reader_rbv/constants.py
+++ reader_rbv/constants.py
@@ -1,3 +1,4 @@
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"
 }
